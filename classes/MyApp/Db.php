e2e3862d3718c4bd09b7b22c0417bd1820e9e08f
naczelnynieuk <naczelnynieuk@gmail.com>
2017-10-29 13:26:25 +0100

    Adding comments to Db.php and stylistics changes in index.php

--- classes/MyApp/Db.php
+++ classes/MyApp/Db.php
@@ -1,14 +1,35 @@
 <?php
 namespace MyApp;
 
+/**
+ *Klasa odpowiadajaca za tworzenie, utrzymyanie polaczenia z baza danych oraz realizacje zapytan;
+ */
 class DB{
+
+/**
+ * Przechowuje instancje klasy DB
+ * @var $_instance
+ */
 	private static $_instance = null;
-	private $_pdo,
-			$_stmt,
-			$_results,
-			$_errors;
+
+/**
+ * Przechowuje obiekt PDO (polaczenie z mysql)
+ * @var _pdo
+ */
+	private $_pdo;
+	private $_stmt;
+
+/**
+ * Przechowuje wyniki zapytania mysql
+ * @var [type]
+ */
+	private $_results;
+	private $_errors;
 			
 
+/**
+ * Tworzy i zapisuje obiekt PDO
+ */
 	private function __construct(){
 		try {
 			$this->_pdo = new \PDO('mysql:host='. Config::get('database/host').';dbname='.Config::get('database/dbname'), Config::get('database/username'), Config::get('database/password'));
@@ -18,6 +39,10 @@ class DB{
 		}
 	}
 
+/**
+ * (Singleton) Sprawdza czy istnieje instancja db i jezeli nie istnieje to tworzy nowa
+ * @return     DB  Instacja kalasy DB.
+ */
 	public static function getInstance(){
 		if (!isset(self::$_instance)) {
 			self::$_instance = new DB;
@@ -25,16 +50,25 @@ class DB{
 		return self::$_instance;
 	}
 
+/**
+ * Czysci instancje po otrzymaniu wynikow z wyslanego zapytania
+ */
 	private function cleanDb(){
 		$this->_stmt = null;
 		$this->_results = null;
 		$this->_errors = null;
 	}
-	//sql - SELECT * FROM Users WHERE Username = ?
 	
-
+/**
+ * 
+ * @param  string $sql        	Zapytanie sql w formie np. "SELECT * FROM username WHERE id = ?"
+ * @param  array  $bindValues 	Tablica wartosci ktore maja zostac zbindowane
+ * @return integer             	Zwraca liczbe rekordow bazy, ktore zareagowaly na zapytanie
+ */
 	private function query($sql, $bindValues){
-		try {
+		$this->cleanDb();
+
+		try {			
 		$bindValuesNumber = count ($bindValues);
 		$this->_stmt = $this->_pdo->prepare($sql);
 		$x =1;
@@ -48,13 +82,19 @@ class DB{
 			return $this->_stmt->rowCount();
 		} catch (\PDOException $e) {
 			$this->_errors[] = 'Blad podczas wykonywania zapytania mysql';
-			return false;
+			return 0;
 		}
 
 
 	}
 
-
+/**
+ * Dodaje dodatkowe parametry na koniec zapytania sql (uzywana w select)
+ * 
+ * @param array $options parametry
+ * 
+ * @return string            	Zwraca sql z dodanymi parametrami
+ */
 	private function addOptions($options){
 		$optionsql ='';
 
@@ -76,10 +116,21 @@ class DB{
 		return $optionsql;
 	}
 
-
-
+/**
+ * Tworzy sql zapytania SELECT i wysyla je do funkcji query wraz z lista parametrow.
+ * Przyklad:
+ * 
+ * <pre>
+ * \MyApp\Db::getInstance()->select('users', ['id', =, 2], ['orderby'=>'id','sort' => 'ASC','limit'=>5]));
+ * </pre>
+ *
+ * @param      string  $from     nazwa tablicy
+ * @param      array   $where    warunek wyszukiwania w bazie
+ * @param      array   $options  dodatkowe parametry na koncu sql
+ *
+ * @return     Zwraca tablice z pobranymi danymi lub null
+ */
 	public function select($from, $where = null, $options = null) {
-		$results = null;
 		$sql = 'SELECT * FROM '. $from;
 		$bindvalue = array();
 
@@ -92,15 +143,24 @@ class DB{
 		}
 
 		if($this->query($sql, $bindvalue)) {
-			$results = $this->_stmt->fetchAll(\PDO::FETCH_ASSOC);
-			$this->cleanDb();
-			return $results;
+			$this->_results = $this->_stmt->fetchAll(\PDO::FETCH_ASSOC);
+			return $this->_results;
 		}
-		$this->cleanDb();
+		return null;
 
 	}
 
-
+/**
+ * Tworzy sql zapytania INSERT i wysyla je do funkcji query wraz z lista parametrow.
+ * Przyklad:
+ * 
+ * <pre>
+ * \MyApp\Db::getInstance()->insert('users', ['username'=> 'marek5','password'=> 'marek4haslo','email'=> 'marek4@gmail.com']);
+ * </pre>
+ * @param  string $into   nazwa tabeli
+ * @param  array $params  tablica kolumna => wartosc
+ * @return integer        Zwraca liczbe rekordow bazy, ktore zareagowaly na zapytanie 
+ */
 	public function insert($into, $params){
 		$bindParams = array();
 		$paramsNumber = count($params);
@@ -127,16 +187,24 @@ class DB{
 			$sql .= '?,';
 		}
 		
-		if($result = $this->query($sql, $bindParams)){
-			$this->cleanDb();
-			return $result;
+		if($this->_results = $this->query($sql, $bindParams)){
+			return $this->_results;
 		}
-		$this->cleanDb();
-		return null;
+		return 0;
 	}
 
-
-
+/**
+ * Tworzy sql zapytania UPADTE i wysyla je do funkcji query wraz z lista parametrow.
+ * Przykad:
+ * 
+ * <pre>
+ * \MyApp\Db::getInstance()->update('users', ['id','=', 1], ['username'=> 'jasiu3','password'=> 'kasztann','email'=> 'jasiu@gmail.com']);
+ * </pre>
+ * @param  string $what   nazwa tabeli
+ * @param  array  $where  warunek wyszukiwana w mysql
+ * @param  array  $params parametry ktore maja zostac zmienione
+ * @return integer         Zwraca liczbe rekordow bazy, ktore zareagowaly na zapytanie 
+ */
 	public function update($what, $where, $params){
 		$bindParams = array();
 		$paramsNumber = count($params);
@@ -156,16 +224,24 @@ class DB{
 		$sql .=' WHERE '.$where[0].$where[1].'?';
 		$bindParams[]= $where[2];
 		
-		if($result = $this->query($sql, $bindParams)){
-			$this->cleanDb();
-			return $result;
+		if($this->_results = $this->query($sql, $bindParams)){
+			return $this->_results;
 		}
 
-		$this->cleanDb();
-		return null;
+		return 0;
 	}
 
-
+/**
+ * Tworzy sql zapytania DELETE i wysyla je do funkcji query wraz z lista parametrow.
+ * Przykad:
+ * 
+ * <pre>
+ * \MyApp\Db::getInstance()->delete('users', ['id','=', 2]);
+ * </pre>
+ * @param  string  $from  nazwa tabeli
+ * @param  array  $where warunek wyszukiwania mysql
+ * @return integer         Zwraca liczbe rekordow bazy, ktore zareagowaly na zapytanie 
+ */
 	public function delete($from, $where){
 		$sql = 'DELETE FROM '. $from;
 		$bindValue = array();
@@ -173,14 +249,15 @@ class DB{
 		$sql .= ' WHERE '. $where[0].$where[1]. '?';
 		$bindValue[] = $where[2];
 		
-		if($result =$this->query($sql, $bindValue)){
-			$this->cleanDb();
-			return $result;
+		if($this->_results =$this->query($sql, $bindValue)){
+			return $this->_results;
 		}
-		$this->cleanDb();
-		return null;
+		return 0;
 	}
 
+/**
+ * Zwraca wynik wynik zapytania do bazy danych
+ */
 	public function getResults(){
 		return $this->_results;
 	}
--- public_html/index.php
+++ public_html/index.php
@@ -4,22 +4,25 @@ require('../core/init.php');
 
 try {
 
+//system szablonow
 $tpl = new \MyApp\Tpl\Engine('../templates/'. \MyApp\Config::get('system/default_template'));
 $view = $tpl->createView(['header', 'index', 'footer']);
+
 
 $view->title = \MyApp\Config::get('system/default_title');
 $view->lang = \MyApp\Config::get('system/default_lang');
 $view->charset = \MyApp\Config::get('system/charset');
-$view->name =  'Zaloguj się lub zarejestruj';
 
+//wiadomosci flash
 $flash = null;
 if ($flash = \MyApp\FlashMessage::render()) {
   $view->flash = $flash;
 }
 
+//dane aktywnego uzytkownika, jezeli istnieje
 $userdata=array();
-
 $user = new \MyApp\User();
+
 if($userdata = $user->getData()){
   $view->user = $userdata;
 }
@@ -30,35 +33,3 @@ $view->render();
 }catch (Exception $e) {
 	die('Wystąpł błąd: '.$e->getMessage());
 }
-
-
-
-
-/*
-
-
-pA( \MyApp\Db::getInstance()->select('users', null, [
-  	'orderby'=>'id',
-  	'sort' => 'ASC',
-  	'limit'=>5
-  	]));
-
- */
-
-
-/*
-echo \MyApp\Db::getInstance()->insert('users', [
- 	'username'=> 'marek5',
- 	'password'=> 'marek4haslo',
- 	'email'=> 'marek4@gmail.com'
- 	]);
-*/
-/*
-echo \MyApp\Db::getInstance()->update('users', ['id','=', 1], [
-  	'username'=> 'jasiu3',
-  	'password'=> 'kasztann',
-  	'email'=> 'jasiu@gmail.com'
-  	]);
- */
-
-// echo \MyApp\Db::getInstance()->delete('users', ['password','=','haslo']);
